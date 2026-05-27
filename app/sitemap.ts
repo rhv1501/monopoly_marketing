ba6060d86dc2180042f6b5e03fed96e6a7afde72
Rudresh H Vyas <129303708+rhv1501@users.noreply.github.com>
2026-05-27 14:21:02 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.monopolymarketing.co.in",
+      url: "https://www.monopolymarketingchennai.in",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://www.monopolymarketing.co.in/thank-you",
+      url: "https://www.monopolymarketingchennai.in",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.1,
